e1f9fe75cf0380de08299a1830edbb27ea2d056f
khallitos <carlphil9924@gmail.com>
2022-07-17 22:11:48 +0000

    Timer folder fix

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Nav, Footer, Sidebar, RightNav, Feed, Quiz,Timer } from "./components";
+import { Box, Button, TextField, Stack } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+// import axios from 'axios';
+
+function App() {
+  return (
+    <Box sx={{ backgroundColor: "" }}>
+      {/* <Nav /> */}
+
+      <Box
+        sx={{
+          marginTop: "80px",
+          paddingX: 30,
+          "@media (max-width: 900px)": {
+            paddingX: 0,
+          },
+          "@media (min-width:900px) and (max-width:1400px)": {
+            paddingX: 15,
+          },
+        }}
+      >
+        {/* <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <RightNav sx={{}} />
+        </Stack> */}
+        {/* <Quiz /> */}
+
+        <Timer/>
+      </Box>
+    </Box>
+  );
+}
+export default App;
